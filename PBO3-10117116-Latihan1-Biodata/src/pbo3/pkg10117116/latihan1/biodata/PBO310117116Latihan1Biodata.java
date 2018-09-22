8a202d51b2532ca58fc0385cb57be0841e1c3a52
muhammadfitrayana <38598539+muhammadfitrayana@users.noreply.github.com>
2018-09-22 11:46:42 +0200

    Add files via upload

---
+++ PBO3-10117116-Latihan1-Biodata/src/pbo3/pkg10117116/latihan1/biodata/PBO310117116Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117116.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA  : Muhammad Fitrayana
+ * Kelas : PBO3
+ * NIM   : 10117116
+ * Deskripsi Tugas : Program Ini Berisi Program Untuk Menampilkan
+ * Value Biodata Ke Layar
+ */
+public class PBO310117116Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "=====================");
+        System.out.println("NAMA\t\t: Muhammad Fitrayana");
+        System.out.println("ALAMAT\t\t: Jalan Unisba IV no 73");
+        System.out.println("TEMPAT LAHIR\t: Bandung ");
+        System.out.println("TANGGAL LAHIR\t: 18 Desember 1998");
+        System.out.println("=====================");
+        System.out.println("HOBI\t\t: Bulutangkis");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Bajigur");
+    }
+    
+}
